9051196bcce0722d01ff55391f4a204d612c6b24
Grady <Gradyhouston@gmail.com>
2018-07-04 01:19:01 -0400

    deploying v1 to heroku

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const chatkit = new Chatkit.default({
 // });
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json());
 app.use(cors());
 

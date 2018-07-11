92795bfe2c7201b4186fb5f3f91374933b865c0c
Grady <Gradyhouston@gmail.com>
2018-07-11 00:33:39 -0400

    rebuild

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const chatkit = new Chatkit.default({
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/index.html', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public'))
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 app.use(bodyParser.urlencoded({ extended: false }))

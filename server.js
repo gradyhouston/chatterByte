0bd57da52681458d3f262ef441da341460e95879
Grady <Gradyhouston@gmail.com>
2018-07-11 00:26:03 -0400

    deploying v1 to heroku

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const chatkit = new Chatkit.default({
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
+app.get('/index.html', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 

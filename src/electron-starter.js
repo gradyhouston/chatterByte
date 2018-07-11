f6d524a4e46d3822ab4e12d8d77bcb72e4f2fc04
Grady <Gradyhouston@gmail.com>
2018-07-11 00:09:02 -0400

    deploying v1 to heroku

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -8,7 +8,7 @@ const url = require('url')
 let mainWindow
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 1630, height: 800 })
+  mainWindow = new BrowserWindow({ width: 1200, height: 800 })
 
   const startUrl =
     process.env.ELECTRON_START_URL ||

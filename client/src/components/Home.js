26f572d2bdff252c0cc158b1be448b1a70f1112e
Grady <Gradyhouston@gmail.com>
2018-07-24 01:16:26 -0400

    pushing logo and new styling to heroku

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import chatterbytelogorecolor from '../chatterbytelogorecolor';
+import chatterbytelogorecolor from './chatterbytelogorecolor';
 export default class Home extends Component {
   render () {
     return (

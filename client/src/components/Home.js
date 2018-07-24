03dbc575d477067332cc30b44d14c28bb6008006
Grady <Gradyhouston@gmail.com>
2018-07-24 01:59:23 -0400

    pushing logo and new styling to heroku

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import chatterbytelogorecolor from '../images/chatterbytelogorecolor.svg';
+import chatterbytelogo from './chatterbytelogo.svg';
+// chatterByte/client/public/images/chatterbytelogorecolor.svg
 export default class Home extends Component {
   render () {
     return (
       <div className="home-welcome">
-        <img src={chatterbytelogorecolor} height="87" width="704" alt="ChatterByte Logo" />
+        <img src={chatterbytelogo} height="87" width="704" alt="ChatterByte Logo" />
         {/* <h4>ChatterByte</h4> */}
           <h5><p>Register.</p>
             <p>Pick a username.</p>
